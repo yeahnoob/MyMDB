e253592bedb1b3697be2a8947bf17236835dba03
Xanadu Zhang <teaeml@gmail.com>
2018-09-05 00:31:24 +0800

    Routing request to the MovieList view

--- mymdb/core/views.py
+++ mymdb/core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from core.models import Movie
+
+
+class MovieList(ListView):
+    model = Movie
